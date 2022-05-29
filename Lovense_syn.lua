3f6a35668b104d82b67d4bb384e9bd7cf0ad4591
Lyssa <35279466+esexual@users.noreply.github.com>
2022-05-28 22:29:09 -0700

    Create Lovense_syn.lua

---
+++ Lovense_syn.lua
@@ -0,0 +1,78 @@
+--[[
+                                       
+,--.                                                
+|  |    ,---.,--.  ,--.,---. ,--,--,  ,---.  ,---.  
+|  |   | .-. |\  `'  /| .-. :|      \(  .-' | .-. : 
+|  '--.' '-' ' \    / \   --.|  ||  |.-'  `)\   --. 
+`-----' `---'   `--'   `----'`--''--'`----'  `----' 
+                                                    
+        Control your Lovense toy with Roblox
+
+]]
+
+local Lovense = {}
+
+local HttpService = game:GetService("HttpService")
+Lovense.debugmode = false;
+Lovense.host;
+
+function Lovense.GetToys()
+    local Response = syn.request({
+        Url = "http://api.lovense.com/api/lan/getToys",
+        Method = "GET"
+    })
+    Lovense.host = Response.domain .. Response.httpsPort;
+    print("Toy(s): " .. Response.toys);
+    print("Host: " .. Lovense.host);
+    if Lovense.debugmode == true then
+        print(Response.Body)
+    else
+        --
+    end
+end
+
+
+
+function Lovense.Domain()
+    print(Lovense.host);
+end
+
+function Lovense.GetBattery()
+    local Response = syn.request({
+        Url = Lovense.host .. "/Battery",
+        Method = "GET"
+    })
+    print("Battery Output: " .. Response.battery);
+end
+
+function Lovense.Vibrate(speed, length)
+    local Response = syn.request({
+        Url = Lovense.host .. "/AVibrate?v=" .. speed .. "&sec=" .. length,
+        Method = "GET"
+    })
+    
+    print("[Lovense] Started vibrating at speed " .. speed .. " for " .. length .. " seconds.");
+
+    if Lovense.debugmode == true then
+        print(Response.Body);
+    else
+        --
+    end
+end
+
+function Lovense.Rotate(speed, length)
+    local Response = syn.request({
+        Url = Lovense.host .. "/ARotate?v=" .. speed .. "&sec=" .. length,
+        Method = "GET"
+    })
+
+    print("[Lovense] Started rotating at speed " .. speed .. " for " .. length .. " seconds.");
+
+    if Lovense.debugmode == true then
+        print(Response.Body)
+    else
+        --
+    end
+end
+
+return Lovense
