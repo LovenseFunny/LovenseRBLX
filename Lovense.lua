6de7df60a4c3036939356fcc1bdf587dd0e01061
Lyssa <35279466+esexual@users.noreply.github.com>
2022-05-28 22:02:34 -0700

    Update Lovense.lua

--- Lovense.lua
+++ Lovense.lua
@@ -13,40 +13,58 @@
 
 local Lovense = {}
 
-Lovense.debugmode = false; -- false by default
-Lovense.host = "";
+local HttpService = game:GetService("HttpService")
+Lovense.debugmode = false;
+Lovense.host;
 
-function Lovense.GetToys()
-    local Response = syn.request({
-        Url = "https://api.lovense.com/api/lan/getToys",
-        Method = "GET"
-    })
-
-    print("[Lovense] Toys received");
-
+function Lovense.Init()
+    local Req = HttpService:GetAsync("http://api.lovense.com/api/lan/getToys")
+    local Response = HttpService:JSONDecode(Req);
+    Lovense.host = Response.domain .. Response.httpsPort;
+    print("Toy(s): " .. Response.toys);
+    print("Host: " .. Lovense.host);
     if Lovense.debugmode == true then
         print(Response.Body)
     else
-        -- Lyssa was here
+        --
     end
+end
 
-    Port = Response.httpPort
-    Domain = Response.domain
-    Toys = Response.toys
+
+
+function Lovense.Domain()
+    print(Lovense.host);
+end
+
+function Lovense.GetBattery()
+    local Req = HttpService:GetAsync(Lovense.host .. "/Battery");
+    local Response = HttpService:JSONDecode(Req);
+    print("Battery Output: " .. Response.battery);
 end
 
 function Lovense.Vibrate(speed, length)
-    local Response = syn.request({
-        Url = Lovense.host .. "/AVibrate?v=" .. speed .. "&sec=" .. length,
-        Method = "GET"
-    })
+    local Req = HttpService:GetAsync(Lovense.host .. "/AVibrate?v=" .. speed .. "&sec=" .. length);
+    local Response = HttpService:JSONDecode(Req);
 
-    print("[Lovense] Started vibrating for " .. length .. " seconds.");
+    print("[Lovense] Started vibrating at speed " .. speed .. " for " .. length .. " seconds.");
+
+    if Lovense.debugmode == true then
+        print(Response.Body);
+    else
+        --
+    end
+end
+
+function Lovense.Rotate()
+    local Req = HttpService:GetAsync(Lovense.host .. "/ARotate?v=" .. speed .. "&sec=" .. length);
+    local Response = HttpService:JSONDecode(Req);
+
+    print("[Lovense] Started rotating at speed " .. speed .. " for " .. length .. " seconds.");
 
     if Lovense.debugmode == true then
         print(Response.Body)
     else
-        -- Lyssa was here
+        --
     end
 end
 
